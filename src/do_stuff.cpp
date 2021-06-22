2a994e5cd9b969a01f01750aa09ca819bb7e04df
Roald Ong <roald.ong@gmail.com>
2021-06-22 14:42:48 +0800

    Removed unused test files.

--- src/do_stuff.cpp
+++
@@ -1,9 +0,0 @@
-#include "lidar_auto_docking/printstuff.h"
-
-int main() {
-  pstuff obj;
-
-  obj.print_stuff();
-  std::cout << print_no();
-  return 0;
-}
--- src/print_stuff.cpp
+++
@@ -1,7 +0,0 @@
-#include "lidar_auto_docking/printstuff.h"
-
-void pstuff::print_stuff() {
-  std::cout << "Stuff printed out from printstuff class!\n";
-}
-
-int print_no() { return 60; }
