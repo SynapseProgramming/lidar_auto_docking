abff34a8adab1aeea57e757f8bb7c6437e5bef9d
Roald Ong <roald.ong@gmail.com>
2021-06-23 19:22:03 +0800

    got shared_ptr test to work.

--- src/sharedptr_test.cpp
+++ src/sharedptr_test.cpp
@@ -5,6 +5,36 @@
 #include "std_msgs/msg/int32.hpp"
 using namespace std::chrono_literals;
 
+class SepPublisher {
+ public:
+  SepPublisher() {}
+
+  void init_node(std::shared_ptr<rclcpp::Node> main_ptr) {
+    main_ptr_ = main_ptr;
+
+    publisher_ =
+        main_ptr_->create_publisher<std_msgs::msg::Int32>("/sepclass_int", 10);
+
+    timer_ = main_ptr_->create_wall_timer(500ms, [this]() {
+      // create string obj.
+      auto message = std_msgs::msg::Int32();
+      message.data = this->count;
+      //  RCLCPP_INFO(main_ptr_->get_logger(), "Publishing: '%d'",
+      //  message.data);
+      // publish message
+      publisher_->publish(message);
+      this->count += 10;
+    });
+  }
+
+ private:
+  int count = 0;
+  // shared ptr of a timer
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_;
+  std::shared_ptr<rclcpp::Node> main_ptr_;
+};
+
 class MinimalPublisher : public rclcpp::Node {
  public:
   MinimalPublisher() : Node("ros_publisher") {
@@ -21,17 +51,30 @@ class MinimalPublisher : public rclcpp::Node {
       this->count++;
     });
   }
+  void init_objects() {
+    std::shared_ptr<rclcpp::Node> new_ptr = shared_ptr_from_this();
+    obj.init_node(new_ptr);
+  }
+
+  std::shared_ptr<rclcpp::Node> shared_ptr_from_this() {
+    return shared_from_this();
+  }
 
  private:
   int count = 0;
   // shared ptr of a timer
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_;
+  SepPublisher obj;
 };
 
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  std::shared_ptr<MinimalPublisher> min_ptr =
+      std::make_shared<MinimalPublisher>();
+  min_ptr->init_objects();
+
+  rclcpp::spin(min_ptr);
   rclcpp::shutdown();
   return 0;
 }
