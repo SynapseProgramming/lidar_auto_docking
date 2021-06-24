4bef3a1cac8e4e491e043f9c58ce17124d7fd439
Roald Ong <roald.ong@gmail.com>
2021-06-24 10:54:26 +0800

    Added in comments into shared_ptr test

--- src/sharedptr_test.cpp
+++ src/sharedptr_test.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <iostream>
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
@@ -9,18 +10,24 @@ class SepPublisher {
  public:
   SepPublisher() {}
 
+  // This function would initialise
   void init_node(std::shared_ptr<rclcpp::Node> main_ptr) {
+    // copy over the main_ptr to a local shared_ptr
     main_ptr_ = main_ptr;
-
+    // init the publisher pointer
     publisher_ =
         main_ptr_->create_publisher<std_msgs::msg::Int32>("/sepclass_int", 10);
 
+    // we would still have to pass (this) pointer to the lamda function to allow
+    // it to access all memeber functions inside the class
     timer_ = main_ptr_->create_wall_timer(500ms, [this]() {
       // create string obj.
       auto message = std_msgs::msg::Int32();
       message.data = this->count;
       //  RCLCPP_INFO(main_ptr_->get_logger(), "Publishing: '%d'",
       //  message.data);
+
+      std::cout << "Sep Class : " << message.data << "\n";
       // publish message
       publisher_->publish(message);
       this->count += 10;
@@ -45,17 +52,23 @@ class MinimalPublisher : public rclcpp::Node {
       // create string obj.
       auto message = std_msgs::msg::Int32();
       message.data = this->count;
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.data);
+      //  RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.data);
+      std::cout << "main class: " << message.data << "\n";
       // publish message
       publisher_->publish(message);
       this->count++;
     });
   }
+
+  // the init_objects function would return a shared_ptr to this class. It will
+  // be stored in new_ptr before being passed to
+  // the init_node function of Sepclass to initialise its memeber functions.
   void init_objects() {
     std::shared_ptr<rclcpp::Node> new_ptr = shared_ptr_from_this();
     obj.init_node(new_ptr);
   }
 
+  // shared_ptr_from_this would return a shared pointer of the current class
   std::shared_ptr<rclcpp::Node> shared_ptr_from_this() {
     return shared_from_this();
   }
@@ -72,6 +85,7 @@ int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   std::shared_ptr<MinimalPublisher> min_ptr =
       std::make_shared<MinimalPublisher>();
+
   min_ptr->init_objects();
 
   rclcpp::spin(min_ptr);
