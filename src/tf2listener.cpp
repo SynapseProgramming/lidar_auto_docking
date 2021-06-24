fd937d530c382ad201d182a7d4433063aa9efeed
Roald Ong <roald.ong@gmail.com>
2021-06-24 16:34:14 +0800

    Renamed parameters.

--- src/tf2listener.cpp
+++ src/tf2listener.cpp
@@ -1,9 +1,10 @@
 #include <lidar_auto_docking/tf2listener.h>
 
-void tf2_listener::waitTransform(std::string source, std::string target) {
+void tf2_listener::waitTransform(std::string origin, std::string destination) {
   std::string warning_msg;
-  while (rclcpp::ok() && !buffer_.canTransform(source, target, tf2::TimePoint(),
-                                               &warning_msg)) {
+  while (rclcpp::ok() &&
+         !buffer_.canTransform(destination, origin, tf2::TimePoint(),
+                               &warning_msg)) {
     std::cout << "waiting: " << warning_msg << "\n";
     rate.sleep();
   }
