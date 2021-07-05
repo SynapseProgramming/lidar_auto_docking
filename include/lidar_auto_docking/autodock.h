fafc5b99aee50b602e4c523262626ab577d2ade2
Roald Ong <roald.ong@gmail.com>
2021-07-05 15:40:44 +0800

    Added in yaml reconfigurable parameters for docking class.

--- include/lidar_auto_docking/autodock.h
+++ include/lidar_auto_docking/autodock.h
@@ -80,7 +80,6 @@ class DockingServer : public rclcpp::Node {
 
  private:
   rclcpp_action::Server<Dock>::SharedPtr action_server_;
-
   rclcpp_action::GoalResponse handle_goal(
       const rclcpp_action::GoalUUID& uuid,
       std::shared_ptr<const Dock::Goal> goal);
