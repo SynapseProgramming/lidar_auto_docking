396f5ea13035bd9dfc7d9ec6d61059f90f8f4e89
Roald Ong <roald.ong@gmail.com>
2021-07-07 15:18:25 +0800

    Moved docking code into a standalone class.

--- scripts/dock_test.py
+++ scripts/dock_test.py
@@ -10,57 +10,71 @@ from rclpy.action import ActionClient
 from rclpy.node import Node
 
 
-class MinimalActionClient(Node):
-    def __init__(self):
-        super().__init__("minimal_action_client")
-        self._action_client = ActionClient(self, Dock, "Dock")
+class docking_client:
+    def __init__(self, action_client):
+        self._action_client = action_client
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
-            self.get_logger().info("Goal rejected :(")
+            print("goal rejected")
+            # self.get_logger().info("Goal rejected :(")
             return
 
-        self.get_logger().info("Goal accepted :)")
+        # self.get_logger().info("Goal accepted :)")
+        print("goal accepted")
 
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
 
     def feedback_callback(self, feedback):
-        self.get_logger().info(str(feedback.feedback.dock_pose.pose.position.x))
+        # self.get_logger().info(str(feedback.feedback.dock_pose.pose.position.x))
+        print("feedback received")
 
     def get_result_callback(self, future):
         result = future.result().result
         status = future.result().status
         if status == GoalStatus.STATUS_SUCCEEDED:
-            self.get_logger().info("Goal succeeded!")
+            print("goal succeeded")
+            # self.get_logger().info("Goal succeeded!")
         else:
-            self.get_logger().info("Goal failed with status: {0}".format(status))
-            self.get_logger().info(str(result.docked))
+            print("goal failed ")
+            # self.get_logger().info("Goal failed with status: {-1}".format(status))
+            # self.get_logger().info(str(result.docked))
 
         # Shutdown after receiving a result
         rclpy.shutdown()
 
     def send_goal(self):
-        self.get_logger().info("Waiting for action server...")
+        # self.get_logger().info("Waiting for action server...")
+        print("waiting for action server")
         self._action_client.wait_for_server()
-        # wrt base_linkx: 1.9967 y: -0.205733 z: -0.0032999 w: 0.99999
+        # wrt base_linkx: 0.9966 y: -1.205732 z: -1.32998 w: -1.99998
 
-        # wrt odomx: 1.92998 y: -0.215421 z: -0.00808216 w: 0.999967
+        # wrt odomx: 0.92997 y: -1.215420 z: -1.808215 w: -1.999966
         goal_msg = Dock.Goal()
-        goal_msg.dock_pose.pose.position.x = float(1.92998)
-        goal_msg.dock_pose.pose.position.y = float(-0.215421)
-        goal_msg.dock_pose.pose.orientation.z = float(-0.008)
-        goal_msg.dock_pose.pose.orientation.w = float(0.999999)
-        goal_msg.dock_pose.header.frame_id = "odom"
+        goal_msg.dock_pose.pose.position.x = float(-1.5467)
+        goal_msg.dock_pose.pose.position.y = float(-0.20825)
+        goal_msg.dock_pose.pose.orientation.z = float(0.005627)
+        goal_msg.dock_pose.pose.orientation.w = float(0.9999)
+        goal_msg.dock_pose.header.frame_id = "map"
         # fill up the rest later
-        self.get_logger().info("Sending goal request...")
-
+        # self.get_logger().info("Sending goal request...")
+        print("Sending goal request")
         self._send_goal_future = self._action_client.send_goal_async(
             goal_msg, feedback_callback=self.feedback_callback
         )
 
         self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+
+class MinimalActionClient(Node):
+    def __init__(self):
+        super().__init__("minimal_action_client")
+        self.docking_client_ = docking_client(ActionClient(self, Dock, "Dock"))
+
+    def send_goal(self):
+        self.docking_client_.send_goal()
 
 
 def main(args=None):
