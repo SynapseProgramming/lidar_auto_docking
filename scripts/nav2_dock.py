f2bb4e664260501a4d50b21c7c9a21dd63c76f5b
Roald Ong <roald.ong@gmail.com>
2021-07-12 14:40:33 +0800

    Added in undocking class and reset step.

--- scripts/nav2_dock.py
+++ scripts/nav2_dock.py
@@ -5,12 +5,66 @@
 from action_msgs.msg import GoalStatus
 from std_msgs.msg import Int32
 from lidar_auto_docking.action import Dock
+from lidar_auto_docking.action import Undock
 from nav2_msgs.action import NavigateToPose
-import json
 
+import json
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
+
+
+class undocking_client:
+    def __init__(self, action_client):
+        self._action_client = action_client
+        self.goal_status = False
+        self.goal_accept_status = False
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            print("Goal Rejected")
+            self.goal_accept_status = False
+            return
+
+        print("Goal accepted")
+        self.goal_accept_status = True
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+
+        result = future.result().result
+        status = future.result().status
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            print("Goal Succeeded!")
+            self.goal_status = True
+            print(str(result.undocked))
+        else:
+            print("Goal Failed!")
+            self.goal_status = False
+
+    def send_goal(self):
+        print("Waiting for action server")
+        self._action_client.wait_for_server()
+        goal_msg = Undock.Goal()
+        goal_msg.rotate_in_place = True
+        print("Sending goal request")
+
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def get_status(self):
+        status = {}
+        status["gs"] = self.goal_status
+        status["gas"] = self.goal_accept_status
+        return status
+
+    def reset_status(self):
+        self.goal_status = False
+        self.goal_accept_status = False
 
 
 class docking_client:
@@ -139,6 +193,8 @@ class MainLogic(Node):
         )
         self.docking_client_ = docking_client(ActionClient(self, Dock, "Dock"))
         self.declare_parameter("load_file_path")
+
+        self.undocking_client_ = undocking_client(ActionClient(self, Undock, "Undock"))
         self.dock_file_path = (
             self.get_parameter("load_file_path").get_parameter_value().string_value
         )
@@ -153,6 +209,7 @@ class MainLogic(Node):
         # update status of all classes
         nav2_status = self.goto_pose_.get_status()
         dock_status = self.docking_client_.get_status()
+        undock_status = self.undocking_client_.get_status()
 
         # TODO: load in the initial dock pose from a json file
         # Docking command received. Navigate to initial goal pose first
@@ -181,6 +238,22 @@ class MainLogic(Node):
         ):
             print("Waiting for undock command!")
             self.dock_stat = 3
+        # Undock command received. Will initiate undocking sequence.
+        elif self.dock_stat == 3 and self.dock_cmd == 2:
+            print("undocking")
+            self.undocking_client_.send_goal()
+            self.dock_stat = 4
+        # Once undocking sequence is complete, reset all boolean flags.
+        elif (
+            self.dock_stat == 4
+            and undock_status["gs"] == True
+            and undock_status["gas"] == True
+        ):
+            print("Docking Sequence Complete")
+            self.dock_stat = 0
+            self.undocking_client_.reset_status()
+            self.docking_client_.reset_status()
+            self.goto_pose_.reset_status()
 
         self.pub_dock_status(self.dock_stat)
 
