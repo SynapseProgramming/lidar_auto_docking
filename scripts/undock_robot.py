cad5014a818c3bef6bdd519b94ba1bc592fb8df6
Roald Ong <roald.ong@gmail.com>
2021-07-12 10:02:08 +0800

    Added in boolean flags into undock class.

--- scripts/undock_robot.py
+++ scripts/undock_robot.py
@@ -13,14 +13,18 @@ from rclpy.node import Node
 class undocking_client:
     def __init__(self, action_client):
         self._action_client = action_client
+        self.goal_status = False
+        self.goal_accept_status = False
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
             print("Goal Rejected")
+            self.goal_accept_status = False
             return
 
         print("Goal accepted")
+        self.goal_accept_status = True
 
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
@@ -31,30 +35,50 @@ class undocking_client:
         status = future.result().status
         if status == GoalStatus.STATUS_SUCCEEDED:
             print("Goal Succeeded!")
+            self.goal_status = True
             print(str(result.undocked))
         else:
             print("Goal Failed!")
+            self.goal_status = False
 
     def send_goal(self):
         print("Waiting for action server")
         self._action_client.wait_for_server()
         goal_msg = Undock.Goal()
         goal_msg.rotate_in_place = True
-        # fill up the rest later
         print("Sending goal request")
 
         self._send_goal_future = self._action_client.send_goal_async(goal_msg)
 
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
+    def get_status(self):
+        status = {}
+        status["gs"] = self.goal_status
+        status["gas"] = self.goal_accept_status
+        return status
+
+    def reset_status(self):
+        self.goal_status = False
+        self.goal_accept_status = False
+
 
 class UndockClient(Node):
     def __init__(self):
         super().__init__("undock_client")
         self.undocking_client_ = undocking_client(ActionClient(self, Undock, "Undock"))
+        self.timer = self.create_timer(0.5, self.timed_callback)
 
     def send_goal(self):
         self.undocking_client_.send_goal()
+
+    def timed_callback(self):
+        undock_status = self.undocking_client_.get_status()
+        print("goal status: " + str(undock_status["gs"]))
+        print("goal accept status: " + str(undock_status["gas"]))
+        if undock_status["gs"] == True and undock_status["gas"] == True:
+            print("Resetting goal status!")
+            self.undocking_client_.reset_status()
 
 
 def main(args=None):
